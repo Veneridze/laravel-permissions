0deb7aef2ce9f51f1d1fda6e94e70ceff1b893ca
Veneridze <viacheslav.kopaev@yandex.ru>
2024-10-05 22:16:48 +0300

    Update Assignable.php

--- src/Interfaces/Assignable.php
+++ src/Interfaces/Assignable.php
@@ -1,5 +1,6 @@
 <?php
 namespace Veneridze\LaravelPermission\Interfaces;
+use Illuminate\Database\Eloquent\Model;
 interface Assignable {
     public function getAccessIds(string $model): array | bool;
     public function canAccess(Model $model): bool;
