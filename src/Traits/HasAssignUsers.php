e24acc3c76fdc5970f1a65f0c2566d3dd3f78b70
Veneridze <viacheslav.kopaev@yandex.ru>
2024-10-07 19:42:05 +0300

    Update HasAssignUsers.php

--- src/Traits/HasAssignUsers.php
+++ src/Traits/HasAssignUsers.php
@@ -1,5 +1,6 @@
 <?php
 namespace Veneridze\LaravelPermission\Traits;
+use Illuminate\Contracts\Support\Arrayable;
 use Illuminate\Database\Eloquent\Collection;
 use Veneridze\LaravelPermission\Models\Role;
 use Veneridze\LaravelPermission\Permission;
@@ -8,7 +9,7 @@ trait HasAssignUsers {
     public function roles(): Collection {
         return Role::where('model_name', app(Permission::class)->getClassName($this::class))->get();
     }
-    public function users(): Collection {
+    public function users(): Arrayable {
         $roles = $this->roles();
         return $roles && $roles->count() > 0 ? User::whereBelongsTo($roles)->get() : collect();
     }
